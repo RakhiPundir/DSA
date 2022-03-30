c9cc3c0ff8c7a96b349140efe6ce8d3cc43b70b0
Rakhi Pundir <90668152+RakhiPundir@users.noreply.github.com>
2022-03-30 12:39:30 +0530

    Create ContainsDuplicates.py

---
+++ Data Structures/Array/ContainsDuplicates.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        for i in nums:
+            if(nums.count(i) >= 2):
+                return True
+            else:
+                continue
+        return False
